1e9100d042fb5e9b3dba7550912bb0f61b24ebb8
Mitchell Holt <mitchellholt@d-i89-171-94.student.eduroam.uq.edu.au>
2024-08-15 14:59:36 +1000

    trying a new algo for w array

--- Serial/fft.c
+++ Serial/fft.c
@@ -4,31 +4,19 @@
 #include "fin_field.h"
 
 
-// Note that this is fast IF we can fit all of w into cache.
-// What happens otherwise?
+// New algrithm does twice as many multiplications, but buffer should be in
+// cache the whole time.
 void primitive_root_powers(int *buffer, int n, int omega) {
-    // fill in first half of the array (multiplications should ONLY be done here)
-    int k = n/2; // number of values to write here
-    int om = omega;
-    buffer[0] = 1;
-    for (int i = 1; i < k; i++) {
+    if (n == 0) return;
+
+    int n2 = n/2;
+    int om = 1;
+    for (int i = 0; i < n2; i++) {
         buffer[i] = om;
         om = mulmod(om, omega);
     }
 
-    // fill in the rest of the values, using those already in the start of the array
-    // TODO: use epic cache stuff to try to keep the parts of w we need in cache
-    uint16_t len = k; len >>= 1; // number of values to write in the current iteration
-    uint16_t shift = 1; // we write the (multiple)th powers of omega in the current iteration
-    // From here, k is the start index of the current block
-    while (len) {
-        for (uint16_t i = 0; i < len; i++) {
-            buffer[k + i] = buffer[i << shift];
-        }
-        k += len;
-        len >>= 1;
-        shift++;
-    }
+    primitive_root_powers(buffer + n, n2, mulmod(omega, omega));
 }
 
 
